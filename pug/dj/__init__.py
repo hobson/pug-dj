1df7040b6b6490bad907a07f738979dc07fbc1e8
Hobson Lane <hobsonlane@gmail.com>
2015-03-13 14:32:31 -0700

    imports in __init__

--- pug/dj/__init__.py
+++ pug/dj/__init__.py
@@ -1,5 +1,17 @@
-# import crawler
-# import crawlnmine
-# import miner
-# import db
-# __all__ = ['db', 'crawler', 'crawlnmine', 'miner']
+from package_info import __license__, __version__, __name__, __doc__, __author__, __authors__
+
+import db
+import explore
+import sqlserver
+import crawler
+import crawlnmine
+import miner
+
+__all__ = [
+    'db',
+    'explore',
+    'sqlserver',
+    'crawler',
+    'crawlnmine',
+    'miner'
+    ]
